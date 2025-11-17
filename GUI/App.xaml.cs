823abfa1f0f21ad761edbfd399a61057a1a507c9
Escartem <escartem.yt@gmail.com>
2025-11-17 02:16:06 -0500

    add logging

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.IO;
+using System.Text;
 using System.Windows;
 using Dark.Net;
 
@@ -16,13 +17,33 @@ namespace GUI
             base.OnStartup(e);
             DarkNet.Instance.SetCurrentProcessTheme(Theme.Dark);
 
-            // TODO: add support for logging to file + console
-            //var logFile = "download.log";
-            //var fileStream = new FileStream(logFile, FileMode.Create, FileAccess.Write, FileShare.Read);
-            //var streamWriter = new StreamWriter(fileStream) { AutoFlush = true };
-            //Console.SetOut(streamWriter);
-            //Console.SetError(streamWriter);
+            var logFile = "latest.log";
+            var fileStream = new FileStream(logFile, FileMode.Create, FileAccess.Write, FileShare.Read);
+            var streamWriter = new StreamWriter(fileStream) { AutoFlush = true };
+
+            var multiWriter = new System.IO.TextWriter[2] { Console.Out, streamWriter };
+            Console.SetOut(TextWriter.Synchronized(new MultiTextWriter(multiWriter)));
+            Console.SetError(TextWriter.Synchronized(new MultiTextWriter(multiWriter)));
+
+
         }
     }
-
+    class MultiTextWriter : TextWriter
+    {
+        private readonly TextWriter[] writers;
+        public MultiTextWriter(TextWriter[] writers) => this.writers = writers;
+        public override Encoding Encoding => Encoding.UTF8;
+        public override void Write(char value)
+        {
+            foreach (var w in writers) w.Write(value);
+        }
+        public override void Write(string value)
+        {
+            foreach (var w in writers) w.Write(value);
+        }
+        public override void WriteLine(string value)
+        {
+            foreach (var w in writers) w.WriteLine(value);
+        }
+    }
 }
